675f73977264df049aed5a60cea611816bac3370
Rafaela da Rosa Soares <89558042+rafaelarosasoares@users.noreply.github.com>
2023-06-23 17:06:53 -0300

    .

--- t1_org.asm
+++ t1_org.asm
@@ -1,9 +1,15 @@
+#
+#	DISASSEMBLER
+#
+#	POR RAFAELA DA ROSA SOARES
+#	ORGANIZACAO DE COMPUTADORES
+#	MATRÍCULA 202211338
+#
+
 .text
 
 .eqv PRINT_INT	1
 .eqv PRINT_STR	4
-.eqv READ_INT	5
-.eqv READ_STR	8
 .eqv EXIT		10
 .eqv PRINT_CHAR	11
 .eqv OPEN_FILE	13
@@ -23,44 +29,35 @@
 
 
 main:
-	addi	$sp, $sp, -12
-	
-	## ARQUIVO DE ENTRADA - ABERTURA
-	addi	$v0, $zero, OPEN_FILE	#diretiva para abertura de arquivo
-	la		$a0, input_file			#arquivo a ser aberto
-	addi	$a1, $zero, 0					#modo de leitura (0)
-	syscall
-	
-	slt   	$t0, $v0, $zero
-    bne   	$t0, $zero, open_error 			
-	
-	sw		$v0, 0($sp)
-	
-	## ARQUIVO DE SAÍDA - ABERTURA
-	
-	add		$v0, $zero, OPEN_FILE	#diretiva para abertura de arquivo
-	la		$a0, output_file		#arquivo a ser aberto
-	addi	$a1, $zero, 1					#modo de escrita (1)
-	syscall
-	
-	slt   	$t0, $v0, $zero
-    bne   	$t0, $zero, open_error
-	
-	sw		$v0, 4($sp)
-	
+	addiu 	$sp, $sp, -12	# Allocate 12 bytes on the stack for 2 file descriptors
+
+    la 		$a0, input_file	# Address of input file name
+    li 		$a1, 0		# Flag for reading
+    li 		$v0, OPEN_FILE		# System call for open
+    syscall
+    sw 		$v0, 0($sp)
+    
+    	slt   	$t0, $v0, $zero
+    	bne   	$t0, $zero, open_error
+    
+   	# Store file descriptor of input file on stack
+
+    la 		$a0, output_file # Address of output file name
+    li 		$a1, 1		# Flag for writing
+    add		$v0, $zero, OPEN_FILE
+    syscall
+	sw 		$v0, -4($sp)	# Store file descriptor of output file on stack
+ 
+       	slt   	$t0, $v0, $zero
+    	bne   	$t0, $zero, open_error
+
 	addi	$t0, $zero, 0x00400000	#primeiro endereço de instrução
 	sw		$t0, 8($sp)
-	
-	addi	$v0, $zero, PRINT_STR
-	la		$a0, sucesso
-	syscall
-	
-	addi	$sp, $sp, 12		#fecha pilha
-	
-	j check_file
-	
+    
+    j 		check_file #lê arquivo de entrada em 4 em 4 bytes
+
 assembly_process:
-	lw			$s5, 0($sp)
+	lw			$s5, -4($sp)
 	srl 		$s4, $s5, 26
 	#descobre opcode
 	
@@ -93,8 +90,7 @@ assembly_process:
 
 
 save_file:
-	addi	$sp, $sp, -12
-	lw		$a0, -8($sp)		#endereço da primeira instrução
+	lw		$a0, 8($sp)		#endereço da primeira instrução
 	lw		$t1, -4($sp)		#endereço do arquivo de saída
 	
 	la		$a1, buffer			#carrega endereço do buffer
@@ -103,7 +99,7 @@ save_file:
 	# carrega e imprime o endereco que esta salvo na pilha
 	move	$a0, $t1			# move o valor do arq de saida para a0
 	la		$a1, buffer			# buffer
-	addi	$a2, $zero, 4		# numero de bytes lidos
+	addi	$a2, $zero, 10		# numero de bytes lidos
 	addi	$v0, $zero, WRITE_FILE	# diretiva de escrita
 	syscall
 	## imprime espaço
@@ -139,8 +135,37 @@ print_assembly:
 	j assembly_process 
 	
 endline_output:
-	addi	$v0, $zero, PRINT_STR
-	la		$a0, str_end_line
+	lw		$a0, -4($sp)
+	la		$a1, str_end_line
+	addi	$a2, $zero, 2
+	addi	$v0, $zero, WRITE_FILE
+	syscall
+	
+check_file:
+	## LÊ ARQUIVO DE ENTRADA - de 4 em 4 bytes
+	lw     	$a0, 0($sp)   # descritor que esta salvo na pilha
+    addiu	$a1, $sp, 4   # endereco onde vai salvar a palavra
+    addi	$a2, $0, 4        # numero de caracteres lidos
+    addi  	$v0, $0, READ_FILE
+    syscall
+
+    # Verifica se foram lidos 4 bytes
+    beq $v0, $zero, close_file  # se não foram lidos nenhum byte, pula para close_file
+
+	j save_file
+	
+close_file:
+	lw		$a0, 0($sp)
+	addi 	$v0, $0, CLOSE_FILE
+	syscall
+	
+	lw 		$a0, -4($sp)
+	addi	$v0, $zero, CLOSE_FILE
+	syscall
+	
+exit:
+	addi	$a0, $zero, 0
+	addi 	$v0, $zero, EXIT_2
 	syscall
 	
 ##############TRATAMENTO DE TIPOS#####################
@@ -196,9 +221,9 @@ type_r:
 	
 ### opcodes  ###
 
-opcode_1:
+	opcode_1:
 
-opcode_1c:
+	opcode_1c:
 
 
 ###  functs  ###
@@ -243,7 +268,7 @@ opcode_1c:
 	
 		lw	$a0, -4($sp)
 		la	$a1, str_addu
-		addi $a2, $zero, 4
+		addi $a2, $zero, 5
 		addi $v0, $zero, WRITE_FILE
 		syscall
 		
@@ -314,7 +339,7 @@ opcode_1c:
 	funct_break:
 		lw	$a0, -4($sp)
 		la	$a1, str_break
-		addi $a2, $zero, 4
+		addi $a2, $zero, 6
 		addi $v0, $zero, WRITE_FILE
 		syscall
 		
@@ -358,7 +383,7 @@ opcode_1c:
 	funct_divu:
 		lw	$a0, -4($sp)
 		la	$a1, str_divu
-		addi $a2, $zero, 4
+		addi $a2, $zero, 5
 		addi $v0, $zero, WRITE_FILE
 		syscall
 		
@@ -393,7 +418,7 @@ opcode_1c:
 	funct_jalr:
 		lw	$a0, -4($sp)
 		la	$a1, str_jalr
-		addi $a2, $zero, 4
+		addi $a2, $zero, 5
 		addi $v0, $zero, WRITE_FILE
 		syscall
 		
@@ -418,7 +443,7 @@ opcode_1c:
 	funct_jr:
 		lw	$a0, -4($sp)
 		la	$a1, str_jr
-		addi $a2, $zero, 4
+		addi $a2, $zero, 3
 		addi $v0, $zero, WRITE_FILE
 		syscall
 		
@@ -432,7 +457,7 @@ opcode_1c:
 	funct_mfhi:
 		lw	$a0, -4($sp)
 		la	$a1, str_mfhi
-		addi $a2, $zero, 4
+		addi $a2, $zero, 5
 		addi $v0, $zero, WRITE_FILE
 		syscall
 		
@@ -444,14 +469,96 @@ opcode_1c:
 		j endline_output
 	
 	funct_mflo:
+		lw	$a0, -4($sp)
+		la	$a1, str_mflo
+		addi $a2, $zero, 5
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		# isola o registrador destino
+		sll $t0, $s5, 16
+		srl $t0, $t0, 27
+		jal check_regs	# identifica os registradores
+		
+		j endline_output
 	
 	funct_mthi:
+		lw	$a0, -4($sp)
+		la	$a1, str_mthi
+		addi $a2, $zero, 5
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		#isola o rs 
+		sll	$t0, $s5, 6
+		srl	$t0, $t0, 27	
+		jal check_regs
+		
+		j endline_output
 	
 	funct_mtlo:
-	
+		lw	$a0, -4($sp)
+		la	$a1, str_mtlo
+		addi $a2, $zero, 5
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		#isola o rs 
+		sll	$t0, $s5, 6
+		srl	$t0, $t0, 27	
+		jal check_regs
+		
+		j endline_output
+		
 	funct_mult:
-	
+		lw	$a0, -4($sp)
+		la	$a1, str_mult
+		addi $a2, $zero, 5
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		#isola o rs 
+		sll	$t0, $s5, 6
+		srl	$t0, $t0, 27	
+		jal check_regs
+		
+		lw	$a0, -4($sp)
+		la	$a1, str_virgula
+		addi $a2, $zero, 1
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		#rt
+		sll	$t0, $s5, 11
+		srl	$t0, $t0, 27
+		jal check_regs	
+		
+		j endline_output
+		
 	funct_multu:
+		lw	$a0, -4($sp)
+		la	$a1, str_multu
+		addi $a2, $zero, 6
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		#isola o rs 
+		sll	$t0, $s5, 6
+		srl	$t0, $t0, 27	
+		jal check_regs
+		
+		lw	$a0, -4($sp)
+		la	$a1, str_virgula
+		addi $a2, $zero, 1
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		#rt
+		sll	$t0, $s5, 11
+		srl	$t0, $t0, 27
+		jal check_regs	
+		
+		j endline_output
 	
 	funct_nor:
 		lw	$a0, -4($sp)
@@ -491,6 +598,109 @@ opcode_1c:
 	funct_or:
 		lw	$a0, -4($sp)
 		la	$a1, str_or
+		addi $a2, $zero, 3
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		# isola o registrador destino
+		sll $t0, $s5, 16
+		srl $t0, $t0, 27
+		jal check_regs	# identifica os registradores
+		
+		lw	$a0, -4($sp)
+		la	$a1, str_virgula
+		addi $a2, $zero, 1
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		#isola o rs 
+		sll	$t0, $s5, 6
+		srl	$t0, $t0, 27	
+		jal check_regs
+		
+		lw	$a0, -4($sp)
+		la	$a1, str_virgula
+		addi $a2, $0, 1
+		addi $v0, $0, WRITE_FILE
+		syscall
+		#rt
+		sll	$t0, $s5, 11
+		srl	$t0, $t0, 27
+		jal check_regs	
+		
+		j endline_output
+	
+	funct_sll:
+		lw $a0, -4($sp)
+		la $a1, str_sll
+		addi $a2, $zero, 4
+		addi $v0, $0, WRITE_FILE
+		
+		##rd
+		sll $t0, $s5, 16
+		srl $t0, $t0, 27
+		jal check_regs	# identifica os registradores
+		
+		lw	$a0, -4($sp)
+		la	$a1, str_virgula
+		addi $a2, $zero, 1
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		#rt
+		sll	$t0, $s5, 11
+		srl	$t0, $t0, 27
+		jal check_regs	
+		
+		#calcula shamt
+		sll $t0, $s5, 11
+		srl $t0, $t0, 27
+		
+		la	$a0, -4($sp)
+		move $a1, $t0
+		addi $a2, $zero, 3
+		addi $v0, $0, WRITE_FILE
+		
+		j endline_output
+
+	funct_sllv:
+		lw	$a0, -4($sp)
+		la	$a1, str_sllv
+		addi $a2, $zero, 5
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		# isola o registrador destino
+		sll $t0, $s5, 16
+		srl $t0, $t0, 27
+		jal check_regs	# identifica os registradores
+		
+		lw	$a0, -4($sp)
+		la	$a1, str_virgula
+		addi $a2, $zero, 1
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		#isola o rs 
+		sll	$t0, $s5, 6
+		srl	$t0, $t0, 27	
+		jal check_regs
+		
+		lw	$a0, -4($sp)
+		la	$a1, str_virgula
+		addi $a2, $0, 1
+		addi $v0, $0, WRITE_FILE
+		syscall
+		#rt
+		sll	$t0, $s5, 11
+		srl	$t0, $t0, 27
+		jal check_regs	
+		
+		j endline_output	
+	
+	funct_slt:
+		lw	$a0, -4($sp)
+		la	$a1, str_slt
 		addi $a2, $zero, 4
 		addi $v0, $zero, WRITE_FILE
 		syscall
@@ -516,22 +726,184 @@ opcode_1c:
 		addi $a2, $0, 1
 		addi $v0, $0, WRITE_FILE
 		syscall
-		
+		#rt
 		sll	$t0, $s5, 11
 		srl	$t0, $t0, 27
 		jal check_regs	
 		
 		j endline_output
 	
-	funct_sll:
-
-	funct_sllv:
-	funct_slt:
 	funct_sltu:
+		lw	$a0, -4($sp)
+		la	$a1, str_sltu
+		addi $a2, $zero, 5
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		# isola o registrador destino
+		sll $t0, $s5, 16
+		srl $t0, $t0, 27
+		jal check_regs	# identifica os registradores
+		
+		lw	$a0, -4($sp)
+		la	$a1, str_virgula
+		addi $a2, $zero, 1
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		#isola o rs 
+		sll	$t0, $s5, 6
+		srl	$t0, $t0, 27	
+		jal check_regs
+		
+		lw	$a0, -4($sp)
+		la	$a1, str_virgula
+		addi $a2, $0, 1
+		addi $v0, $0, WRITE_FILE
+		syscall
+		#rt
+		sll	$t0, $s5, 11
+		srl	$t0, $t0, 27
+		jal check_regs	
+		
+		j endline_output
+	
 	funct_sra:
+		lw $a0, -4($sp)
+		la $a1, str_sra
+		addi $a2, $zero, 4
+		addi $v0, $0, WRITE_FILE
+		
+		##rd
+		sll $t0, $s5, 16
+		srl $t0, $t0, 27
+		jal check_regs	# identifica os registradores
+		
+		lw	$a0, -4($sp)
+		la	$a1, str_virgula
+		addi $a2, $zero, 1
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		#rt
+		sll	$t0, $s5, 11
+		srl	$t0, $t0, 27
+		jal check_regs	
+		
+		#calcula shamt
+		sll $t0, $s5, 11
+		srl $t0, $t0, 27
+		
+		la	$a0, -4($sp)
+		move $a1, $t0
+		addi $a2, $zero, 3
+		addi $v0, $0, WRITE_FILE
+		
+		j endline_output
+	
 	funct_srav: 
+		lw	$a0, -4($sp)
+		la	$a1, str_srav
+		addi $a2, $zero, 5
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		# isola o registrador destino
+		sll $t0, $s5, 16
+		srl $t0, $t0, 27
+		jal check_regs	# identifica os registradores
+		
+		lw	$a0, -4($sp)
+		la	$a1, str_virgula
+		addi $a2, $zero, 1
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		#isola o rs 
+		sll	$t0, $s5, 6
+		srl	$t0, $t0, 27	
+		jal check_regs
+		
+		lw	$a0, -4($sp)
+		la	$a1, str_virgula
+		addi $a2, $0, 1
+		addi $v0, $0, WRITE_FILE
+		syscall
+		#rt
+		sll	$t0, $s5, 11
+		srl	$t0, $t0, 27
+		jal check_regs	
+		
+		j endline_output
+	
 	funct_srl:
+		lw $a0, -4($sp)
+		la $a1, str_srl
+		addi $a2, $zero, 4
+		addi $v0, $0, WRITE_FILE
+		
+		##rd
+		sll $t0, $s5, 16
+		srl $t0, $t0, 27
+		jal check_regs	# identifica os registradores
+		
+		lw	$a0, -4($sp)
+		la	$a1, str_virgula
+		addi $a2, $zero, 1
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		#rt
+		sll	$t0, $s5, 11
+		srl	$t0, $t0, 27
+		jal check_regs	
+		
+		#calcula shamt
+		sll $t0, $s5, 11
+		srl $t0, $t0, 27
+		
+		la	$a0, -4($sp)
+		move $a1, $t0
+		addi $a2, $zero, 3
+		addi $v0, $0, WRITE_FILE
+		
+		j endline_output
+	
+	
 	funct_srlv:
+		lw	$a0, -4($sp)
+		la	$a1, str_srlv
+		addi $a2, $zero, 5
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		# isola o registrador destino
+		sll $t0, $s5, 16
+		srl $t0, $t0, 27
+		jal check_regs	# identifica os registradores
+		
+		lw	$a0, -4($sp)
+		la	$a1, str_virgula
+		addi $a2, $zero, 1
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		#isola o rs 
+		sll	$t0, $s5, 6
+		srl	$t0, $t0, 27	
+		jal check_regs
+		
+		lw	$a0, -4($sp)
+		la	$a1, str_virgula
+		addi $a2, $0, 1
+		addi $v0, $0, WRITE_FILE
+		syscall
+		#rt
+		sll	$t0, $s5, 11
+		srl	$t0, $t0, 27
+		jal check_regs	
+		
+		j endline_output	
 	
 	funct_sub: 
 		lw	$a0, -4($sp)
@@ -603,50 +975,134 @@ opcode_1c:
 		
 		j endline_output
 	
-	
 	funct_syscall:
+		lw	$a0, -4($sp)
+		la	$a1, str_syscall
+		addi $a2, $zero, 8
+		addi $v0, $zero, WRITE_FILE
+		syscall
+	
 	funct_xor:
-
+		lw 	$a0, -4($sp)
+		la	$a1, str_xor
+		addi $a2, $zero, 4
+		addi $v0, $0, WRITE_FILE
+		syscall
+		
+		# isola o registrador destino
+		sll $t0, $s5, 16
+		srl $t0, $t0, 27
+		jal check_regs	# identifica os registradores
+		
+		lw	$a0, -4($sp)
+		la	$a1, str_virgula
+		addi $a2, $zero, 1
+		addi $v0, $zero, WRITE_FILE
+		syscall
+		
+		#isola o rs 
+		sll	$t0, $s5, 6
+		srl	$t0, $t0, 27	
+		jal check_regs
+		
+		lw	$a0, -4($sp)
+		la	$a1, str_virgula
+		addi $a2, $0, 1
+		addi $v0, $0, WRITE_FILE
+		syscall
+		
+		sll	$t0, $s5, 11
+		srl	$t0, $t0, 27
+		jal check_regs	
+		
+		j endline_output
 
 #############TIPO J###################
-type_j_jal:
-
-type_j_j:
-
+	type_j_jal:
+		lw $a0, -4($sp)
+		la $a1, str_jal
+		addi $a2, $0, 4
+		addi $v0, $0, WRITE_FILE
+		syscall
+	
+		j check_j_address
+	
+	type_j_j:
+		lw $a0, -4($sp)
+		la $a1, str_j
+		addi $a2, $0, 4
+		addi $v0, $0, WRITE_FILE
+		syscall
+	
+		j check_j_address
 
 ##############TIPO I###################
-type_i:
+	type_i:
 
+
+
+######## registradores, imediates & endereço de procedimentos #######
+
+check_j_address:
+
+	sll	$t1, $s5, 6
+	srl $t1, $t1, 6
+	
+	move $a0, $t1
+	la 	$a1, buffer
+	jal to_string
+	
+	lw 	$a0, -4($sp)
+	la 	$a1, buffer
+	addi $a2, $0, 10
+	addi $v0, $zero, WRITE_FILE
+	syscall
+	
+	j endline_output
 
 check_regs:
 	beq	$t0, 0x00, reg00	#zero
 	beq	$t0, 0x01, reg01 	#at
 	beq $t0, 0x02, reg02
+	beq $t0, 0x03, reg03
+	beq $t0, 0x04, reg04
+	beq $t0, 0x05, reg05
+	beq $t0, 0x06, reg06
+	beq $t0, 0x07, reg07
+	beq $t0, 0x08, reg08
+	beq $t0, 0x09, reg09
 	
 	
 	
 reg00:
-reg01:
-reg02:
-	
-check_file:
-	## LÊ ARQUIVO DE ENTRADA - de 4 em 4 bytes
-	lw		$t0, 0($sp)		#carrega em $t0 o arquivo de input
-	move	$a0, $t0
-	addiu	$a1, $sp, 4		# onde vai ser salvo 
-	addi	$a2, $zero, 4
-	addi	$v0, $zero, READ_FILE
-	syscall
-	
-	slti	$t0, $a0, 4	#checa se 4 bytes foram lidos
-	beq		$t0, $0, save_file		# se sim, salva arquivo
-	# se nao
 
-close_file:
-	
-exit:
-	addi 	$v0, $zero, EXIT_2
-	syscall
+		lw $a0, -4($sp)
+		la $a1, str_r0
+		addi $a2, $0, 5
+		addi $v0, $0, WRITE_FILE
+		syscall
+
+reg01:
+		lw $a0, -4($sp)
+		la $a1, str_r1
+		addi $a2, $0, 3
+		addi $v0, $0, WRITE_FILE
+		syscall
+
+reg02:
+		lw $a0, -4($sp)
+		la $a1, str_r2
+		addi $a2, $0, 3
+		addi $v0, $0, WRITE_FILE
+		syscall
+
+reg03:
+reg04:
+reg05:
+reg06:
+reg07:
+reg08:
+reg09:
 
 open_error:
 	addi	$v0, $zero, PRINT_STR
@@ -654,7 +1110,6 @@ open_error:
 	syscall
 	
 	j exit
-	
 	
 to_string:
 	#a0 -> valor para conversão
@@ -702,6 +1157,7 @@ error_open:			.asciiz "Erro na abertura do arquivo.\n"
 sucesso:			.asciiz "sucesso\n"
 
 buffer:				.space	128
+fd:					.space 	32
 
 ## basicos
 hex_digits:			.asciiz "0123456789ABCDEF"
@@ -740,6 +1196,13 @@ str_sub:			.asciiz "sub "			#100010
 str_subu:			.asciiz "subu "			#100011
 str_syscall:		.asciiz "syscall "		#001100
 str_xor:			.asciiz "xor "			#100110
+
+
+## type j
+
+str_jal:			.asciiz "jal "			#000011
+str_j:				.asciiz "j "			#000010
+
 
 ## registradores
 
